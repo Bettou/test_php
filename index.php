76bb7e463bf544528fc662d6e78ad6d5a4926418
Bettou <omaridrissa53@gmail.com>
2022-06-02 16:47:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+<h1>Donation camp</h1>
+<body bgcolor="FBB917">
+    <div> <h2>Registration Form</h2></div>
+    <form action="connect.php" method="POST">
+        <label for="user">Name:</label> <br>
+        <input type="text" name="name" id="user"> <br> <br>
+        <label for="email">Email:</label> <br>
+        <input type="email" name="email" id="email"><br> <br>
+        <label for="phone">Phone:</label> <br>
+        <input type="phone" name="phone" id="phone"><br> <br>
+        <label for="bgroup">Blood group:</label> <br>
+        <input type="text" name="bgroup" id="bgroup"><br> <br>
+        <input type="submit" name="submit" id="submit">
+
+    </form>
+</body>
+</html>
+
+
+
+<?php
+
+
+
+?>
